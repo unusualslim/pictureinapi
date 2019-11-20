ca1905fca1e3e31c5333681aa323aed5e3a64cee
dev <dev@printrdwn.com>
2019-11-20 20:19:10 +0000

    doda

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,9 +29,6 @@ Rails.application.configure do
   # Do fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  config.assets.debug = true
-
-
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
