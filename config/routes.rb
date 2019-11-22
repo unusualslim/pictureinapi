29fa30af00483ffee57cddd2ccae9fb650b49def
unusualslim <lance@perrybrothersoil.com>
2019-11-22 21:41:18 +0000

    v1.0

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   root :to => "pages#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :boards, except: :show
+  resources :boards #, except: :show
     
-  get 'users/:user_id/boards/:id', to: 'boards#show'
+#  get 'users/:user_id/boards/:id', to: 'boards#show'
   
 #  resources :users do
 #    resources :boards, only: :show
