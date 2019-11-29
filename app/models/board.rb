a8d033a1edb2a22df79fd0430e122127fc4671d7
unusualslim <lance@perrybrothersoil.com>
2019-11-29 20:27:13 +0000

    update slug column on name change

--- app/models/board.rb
+++ app/models/board.rb
@@ -7,4 +7,10 @@ class Board < ApplicationRecord
   whitelist = ['image/png', 'image/jpeg', 'image/gif', 'video/mp4', 'application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.presentationml.presentation' ]
 
   validates :image, presence: true, blob: { content_type: whitelist}
+  
+  def should_generate_new_friendly_id?
+    slug.blank?  || name_changed?
+  end
+
+
 end
