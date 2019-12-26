455c661bfe5e3a528925dc9843eeb460eea34d84
unusualslim <lance@perrybrothersoil.com>
2019-12-26 09:28:34 -0500

    doda

--- app/models/board.rb
+++ app/models/board.rb
@@ -4,9 +4,14 @@ class Board < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
 
-  whitelist = ['image/png', 'image/jpeg', 'image/gif', 'video/mp4', 'application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.presentationml.presentation' ]
-
+  whitelist = ['image/png', 'image/jpeg', 'image/gif', 'video/mp4', 'application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.presentationml.presentation', 'video/x-matroska' ]
+ 
   validates :image, presence: true, blob: { content_type: whitelist}
+
+# Blacklist:
+#   video/x-msvideo  # AVI
+#   video/x-flv  # FLV
+
   
   def should_generate_new_friendly_id?
     slug.blank?  || name_changed?
