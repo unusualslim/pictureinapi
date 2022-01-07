c6ebdd5d84962d63a2f34faf533ff6d8e1d1a74c
unusualslim <lance@perrybrothersoil.com>
2022-01-07 14:54:47 -0500

    capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :puma_threads,	[4,16]
 set :puma_workers, 	0
 set :rbenv_ruby, 	'2.6.5'
 set :rbenv_custom_path, "/home/dev/.rbenv"
-set :linked_files, %w{config/master.key}
+#set :linked_files, %w{config/master.key}
 
 
 # Default branch is :master
@@ -29,7 +29,7 @@ set :linked_files, %w{config/master.key}
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
@@ -51,4 +51,18 @@ namespace :deploy do
     end
   end
 
+end
+
+append :linked_files, "config/master.key"
+
+namespace :deploy do
+  namespace :check do
+    before :linked_files, :set_master_key do
+      on roles(:app), in: :sequence, wait: 10 do
+        unless test("[ -f #{shared_path}/config/master.key ]")
+          upload! 'config/master.key', "#{shared_path}/config/master.key"
+        end
+      end
+    end
+  end
 end
