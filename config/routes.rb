5eb8c277bf9741c5ad07943c2f9f4e9efe24dab1
AbdulMeher <Abdulmeher0823@gmail.com>
2022-01-24 08:34:05 -0500

    Updating the version and deployment to containers

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
  
   root  "pages#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :boards #, except: :show
-  resources :articles    
+  resources :articles
   get 'users/:user_id/boards/:id', to: 'boards#show'
-  
+
   get "/:page" => "pages#show"
 
 #  resources :users do
